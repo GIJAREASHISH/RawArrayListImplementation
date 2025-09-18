f38dda26061dfbe972d3eb1572c84270de24752f
Ashish Gijare <ashgijare@gmail.com>
2025-09-18 10:06:12 +0530

    Removed unnecessary call

---
+++ NestedInterface/Test.java
@@ -0,0 +1,12 @@
+package NestedInterface;
+
+public class Test extends B {
+
+    public static void main(String[] args) {
+        B objb = new B();
+
+        System.out.println(objb.isOdd(2));
+
+    }
+
+}
