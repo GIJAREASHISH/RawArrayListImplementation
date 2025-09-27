bb3035bf63553fce34acaf12a8a920b78d8a3421
Ashish Gijare <ashgijare@gmail.com>
2025-09-27 23:36:24 +0530

    added resize logic

--- RawHashMapImplementation/MyHashMapclosetoActual.java
+++ RawHashMapImplementation/MyHashMapclosetoActual.java
@@ -3,16 +3,21 @@ package RawHashMapImplementation;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+
+
 public class MyHashMapclosetoActual<K,V> {
 
     int size;
+    int capacity=16;
+    float loadFactor = 0.75f;
     //Array of buckets, Used linkedlist to overcome collision
     LinkedList<Entry<K,V>>[] table;
+    LinkedList<Entry<K,V>>[] newtable;
 
     //Constructor fixed size linked list initially
     MyHashMapclosetoActual(){
 
-        this.table = new LinkedList[10];
+        this.table = new LinkedList[capacity];
         System.out.println(this.table.length);
         this.size=0;
     }
@@ -41,7 +46,7 @@ public class MyHashMapclosetoActual<K,V> {
 
         int index =key.hashCode()%table.length;
 
-        if(size==table.length){
+        if(size==table.length*loadFactor){
             resize(key,value);
         }
 
@@ -62,16 +67,30 @@ public class MyHashMapclosetoActual<K,V> {
 
     /*Wrong Resize Implementation*/
     public void resize(K key, V v){
-            int newCapacity=table.length*2;
-        int index =key.hashCode()%table.length;
+            int newCapacity=capacity*2;
+
             LinkedList<Entry<K,V>>[] newtable= new LinkedList[newCapacity];
 
-            for(Entry<K,V> entry: table[index]){
-
-                newtable=table;
+            //Initialize new bucket
+            for(int i=0;i<newCapacity;i++){
+                newtable[i]=new LinkedList<>();
             }
 
+            //Copy all enties
+            for(LinkedList<Entry<K,V> >bucket: table){
 
+               for(Entry<K,V> entry: bucket){
+
+                   int newIndex=(entry.key.hashCode())%newCapacity;
+
+                   newtable[newIndex].add(entry);
+
+
+               }
+            }
+
+        table=newtable;
+            capacity=newCapacity;
     }
 
     public V get(K key){
